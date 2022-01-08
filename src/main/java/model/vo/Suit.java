e28e23e568fa49cccee34b6943bfe2c8aa880d2c
cjh8746 <cjh87467@gmail.com>
2022-01-08 14:36:53 +0900

    feat : Suit 구현

---
+++ src/main/java/model/vo/Suit.java
@@ -0,0 +1,18 @@
+package model.vo;
+
+public enum Suit {
+    SPADE("spade"),
+    HEART("heart"),
+    DIAMOND("diamond"),
+    CLUB("club");
+
+    private final String value;
+
+    Suit(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
