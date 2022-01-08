73e98a8be26b72a01ee05825157a020e6fe3a29d
cjh8746 <cjh87467@gmail.com>
2022-01-08 14:36:28 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/model/vo/SuitTest.java
@@ -0,0 +1,22 @@
+package model.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SuitTest {
+    @Test
+    @DisplayName("getValue() 호출 시, enum 이 가지고 있는 값을 반환한다.")
+    void getValue() {
+        //given
+        Suit suit = Suit.HEART;
+        String expectedSuit = "heart";
+
+        //when
+        String actual = suit.getValue();
+
+        //then
+        assertThat(actual).isEqualTo(expectedSuit);
+    }
+}
