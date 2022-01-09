d096377f01d541d1cb6bf44363e9e597acfbd515
cjh8746 <cjh87467@gmail.com>
2022-01-09 17:00:26 +0900

    refactor : getNumberValue() 메서드 추가

--- src/main/java/model/vo/PlayingCard.java
+++ src/main/java/model/vo/PlayingCard.java
@@ -23,6 +23,10 @@ public class PlayingCard {
         return this.number;
     }
 
+    public int getNumberValue() {
+        return this.number.getValue();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
