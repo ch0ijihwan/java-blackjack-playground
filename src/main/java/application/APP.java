b88b180fd6caa159d13cacd6fcccd9d6de118e31
cjh8746 <cjh87467@gmail.com>
2022-01-14 16:15:27 +0900

    feat : 앱 실행을 위한 APP 객체 생성

---
+++ src/main/java/application/APP.java
@@ -0,0 +1,9 @@
+package application;
+
+import controller.Controller;
+
+public class APP {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+}
