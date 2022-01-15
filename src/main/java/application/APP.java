15ec81c6ededc58949266e9ba09b38c0c53f3466
cjh8746 <cjh87467@gmail.com>
2022-01-15 23:38:16 +0900

    refactor : 구현체만 생성되도록 변경

--- src/main/java/application/APP.java
+++ src/main/java/application/APP.java
@@ -4,6 +4,6 @@ import controller.Controller;
 
 public class APP {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        new Controller();
     }
 }
