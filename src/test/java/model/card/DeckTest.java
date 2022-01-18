02fc1e836c2551af8df985fc50fc03069588181c
cjh8746 <cjh87467@gmail.com>
2022-01-18 10:49:53 +0900

    test : 예외처리 테스트 코드 추가

--- src/test/java/model/card/DeckTest.java
+++ src/test/java/model/card/DeckTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class DeckTest {
+    private static final int FIRST_INDEX_OF_DECK = 0;
+    private static final int LAST_INDEX_OF_DECK = 52;
     @Test
     @DisplayName("객체 생성 시, 카드를 52장 생성한다.")
     void createDeck() {
@@ -28,20 +32,33 @@ class DeckTest {
     }
 
     @Test
-    @DisplayName("draw() 호출 시, 덱에서 카드 한장을 뽑아 카드 객체를 반환한다.")
-    void draw() {
+    @DisplayName("provideCard() 호출 시, 덱에서 카드 한장을 뽑아 카드 객체를 반환한다.")
+    void provideCard() {
         //given
         Deck deck = new Deck();
         int expectedNumberOfCards = 51;
 
         //when
-        PlayingCard card = deck.draw();
+        PlayingCard card = deck.provideCard();
 
         //then
         assertThat(deck).extracting("cards")
                 .asList()
                 .size()
                 .isEqualTo(expectedNumberOfCards);
+    }
+
+    @Test
+    @DisplayName("provideCard() 호출 시, 덱에 카드가 존재하지 않는 다면 예외처리를 반환한ㄷ..")
+    void provideCardWhenNoCard() {
+        //given
+        Deck deck = new Deck();
+        IntStream.range(FIRST_INDEX_OF_DECK, LAST_INDEX_OF_DECK)
+                .forEach(time -> deck.provideCard());
+
+        //then
+        assertThatThrownBy(deck::provideCard).isInstanceOf(IllegalStateException.class)
+                .hasMessage("덱에 카드가 없는 상태입니다. 카드를 뽑을 수 없습니다.");
     }
 
     @Test
